81d6c9f24017e9d15fcd4adf84e583b8e4c07b14
gokilavani-dev <gokilavani2k@gmail.com>
2025-11-24 07:26:36 -0800

    Enhanced UI

---
+++ js/book.js
@@ -0,0 +1,6 @@
+
+const fixPlugin = () => {
+    document.body.classList.remove("iti-mobile");
+};
+
+setInterval(fixPlugin, 50); 
--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-$(document).ready(function() {
-  $('ul.navbar-nav .nav-link').click(function() {
-    // Remove active class from all nav-links
-    $('ul.navbar-nav .nav-link').removeClass('active');
-    // Add active class to the clicked nav-link
-    $(this).addClass('active');
-  });
-});
-document.querySelectorAll(".navbar-nav .nav-link").forEach(function (link) {
-    link.addEventListener("click", function () {
-        let navbarToggler = document.querySelector(".navbar-collapse");
-        if (navbarToggler.classList.contains("show")) {
-            new bootstrap.Collapse(navbarToggler).hide();
-        }
-    });
+
+document.addEventListener("scroll", function () {
+
+  const homeLink = document.querySelector('a[href="index.html"]');
+  const aboutLink = document.querySelector('a[href="index.html#aboutus"]');
+  const servicesLink = document.querySelector('a[href="index.html#services"]');
+
+  const about = document.getElementById("aboutus");
+  const services = document.getElementById("services");
+
+  let y = window.scrollY + 100;
+
+  // Remove all active
+  homeLink.classList.remove("active");
+  aboutLink.classList.remove("active");
+  servicesLink.classList.remove("active");
+
+  if (y < about.offsetTop) {
+    homeLink.classList.add("active");
+  } 
+  else if (y >= about.offsetTop && y < services.offsetTop) {
+    aboutLink.classList.add("active");
+  } 
+  else {
+    servicesLink.classList.add("active");
+  }
+
 });
