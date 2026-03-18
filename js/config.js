278204f9836e369317f2bace24301abda30b9a60
gokilavani-dev <gokilavani2k@gmail.com>
2026-03-18 15:15:22 +0530

    Remove config.js from tracking

--- js/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-        apiKey: "AIzaSyAHGBdgyg21jfj_2uHHuPIIo2GE1oPf1DU",
-        authDomain: "vagr-residency.firebaseapp.com",
-        projectId: "vagr-residency",
-        storageBucket: "vagr-residency.firebasestorage.app",
-        messagingSenderId: "906241851144",
-        appId: "1:906241851144:web:1322d674bd92f62aab36bd"
-      };
-export const razorpayKey = "rzp_test_SFDTUY7FTRB5pp";
